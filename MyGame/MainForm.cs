8f1ffb723048ae5d079217ef2bd8818c5a71a6d3
Scheik <scheik.todeswache@googlemail.com>
2015-12-30 19:24:52 +0100

    Pfeiltasten mit Texturen

--- MyGame/MainForm.cs
+++ MyGame/MainForm.cs
@@ -13,7 +13,6 @@ namespace MyGame
 {
     public partial class MainForm : Form
     
-
     {
         private Game game = new Game();
         private Stopwatch watch = new Stopwatch();
@@ -26,6 +25,29 @@ namespace MyGame
             game.PlaygroundSize = new Point(renderControl.ClientSize.Width, renderControl.ClientSize.Height);
         }
 
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (game != null)
+            {
+                switch (keyData)
+                {
+                    case Keys.Left:
+                        game.Left = true;
+                        break;
+                    case Keys.Right:
+                        game.Right = true;
+                        break;
+                    case Keys.Up:
+                        game.Up = true;
+                        break;
+                    case Keys.Down:
+                        game.Down = true;
+                        break;
+                }
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
         private void timer_Tick(object sender, EventArgs e)
         {
             game.Update(watch.Elapsed);
@@ -36,6 +58,30 @@ namespace MyGame
         private void closeToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            if (game == null)
+                return;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    game.Left = false;
+                    break;
+                case Keys.Right:
+                    game.Right = false;
+                    break;
+                case Keys.Up:
+                    game.Up = false;
+                    break;
+                case Keys.Down:
+                    game.Down = false;
+                    break;
+            }
+
+            base.OnKeyUp(e);
         }
     }
 }
